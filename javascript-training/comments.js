41888e3fba891c5d280d7fcb81dc12b5f91274ab
Deekshitha <deekshitha017@gmail.com>
2026-06-24 14:50:17 -0500

    Assignment 1

---
+++ javascript-training/comments.js
@@ -0,0 +1,26 @@
+console.log("Hello World!")
+
+// (ctrl+/) to comment out a line of code in most code editors
+// This is a comment in JavaScript. It is ignored by the JavaScript engine and is used to explain the code or leave notes for other developers.
+
+//(Alt+Shift+A) to create a multi-line comment in most code editors
+
+/* This is a multi-line comment in JavaScript. 
+It can span multiple lines and is also ignored by the JavaScript engine. 
+It is useful for writing longer explanations or commenting out blocks of code. */
+
+let a = 10;
+let b = 20;
+
+console.log(a++);
+console.log(a);
+console.log(b++);
+console.log(b);
+ 
+console.log(++a);
+console.log(a);
+console.log(++b);
+console.log(b);   
+
+
+
